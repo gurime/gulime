a73cbbe38baf16386bd06ce7c065583c6ef4596a
Phillip Bailey <ztron500@gmail.com>
2024-04-24 15:08:15 -0400

    Update Dashboard.tsx

--- app/components/Dashboard.tsx
+++ app/components/Dashboard.tsx
@@ -155,51 +155,32 @@ export default function Dashboard() {
 
 
   const handleAddToCart = async (article: Article) => {
-    try {
-      const auth = getAuth();
-      const currentUser = auth.currentUser;
-  
-      if (currentUser) {
-        const db = getFirestore();
-        const cartRef = doc(db, 'Cart', currentUser.uid);
-        const cartData = await getDoc(cartRef);
-  
-        if (cartData.exists()) {
-          const existingItems = cartData.data().items;
-          existingItems.push({
-            id: article.id || '',
-            title: article.title || '',
-            price: article.price || '',
-            coverimage: article.coverimage || '',
-            // Add any other relevant properties with default values
-          });
-          await setDoc(cartRef, { items: existingItems }, { merge: true });
-        } else {
-          // If the cart document doesn't exist, create a new one
-          const newCartData = {
-            userId: currentUser.uid,
-            items: [
-              {
-                id: article.id || '',
-                title: article.title || '',
-                price: article.price || '',
-                coverimage: article.coverimage || '',
-                // Add any other relevant properties with default values
-              }
-            ]
-          };
-          await setDoc(cartRef, newCartData);
-        }
-  
-        console.log('Item added to cart successfully!');
-        router.push('/pages/Cart'); // Navigate to the cart page
-      } else {
-        console.log('User not authenticated');
-      }
-    } catch (error) {
-      console.error('Error adding item to cart:', error);
+  try {
+    const auth = getAuth();
+    const currentUser = auth.currentUser;
+    const db = getFirestore();
+
+    if (currentUser) {
+      const cartRef = doc(db, 'Cart', currentUser.uid);
+      const itemRef = doc(cartRef, 'items', article.id);
+
+      await setDoc(itemRef, {
+        id: article.id,
+        title: article.title,
+        price: article.price,
+        coverimage: article.coverimage,
+        // Add any other relevant properties
+      });
+
+      console.log('Item added to cart successfully!');
+      router.push('/pages/Cart'); // Navigate to the cart page
+    } else {
+      console.log('User not authenticated');
     }
-  };
+  } catch (error) {
+    console.error('Error adding item to cart:', error);
+  }
+};
 
 return (
 <>
