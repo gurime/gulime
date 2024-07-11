3b3e5cd683da21da047c3ba47139f47df072f162
Phillip Bailey <ztron500@gmail.com>
2024-07-11 19:40:51 -0400

    Update firebase.js

--- app/firebase/firebase.js
+++ app/firebase/firebase.js
@@ -5,15 +5,16 @@ import { getAuth } from 'firebase/auth';
 
 
 
-const firebaseConfig = {
-    apiKey: "AIzaSyBV5EoulFpeurXg5l6mrwYDTzu5wQjY_-0",
-    authDomain: "gulime.firebaseapp.com",
-    projectId: "gulime",
-    storageBucket: "gulime.appspot.com",
-    messagingSenderId: "761074487006",
-    appId: "1:761074487006:web:84c2cb0f4e55694520bc4f"
-};
 
+
+const firebaseConfig = {
+    apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+    authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+    projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+    storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+    appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+  };
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 const db = getFirestore(app);
