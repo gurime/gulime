288c082b403ab76fc8250ed5698e7dcc9aa168d3
Phillip Bailey <ztron500@gmail.com>
2024-08-26 19:04:52 -0400

    update

--- app/components/HomePage.jsx
+++ app/components/HomePage.jsx
@@ -7,112 +7,114 @@ import { RiseLoader } from 'react-spinners';
 import Carousel from '../components/HomeCarousel '
 import { useFirebaseFeaturedProducts } from '../hooks/CarouselHooks';
 async function getArticles() {
-  try {
-    const querySnapshot = await getDocs(collection(db, "products"));
-    return querySnapshot.docs.map(doc => ({id: doc.id, ...doc.data()}));
-  } catch (error) {
-    throw error;
-  }
+try {
+const querySnapshot = await getDocs(collection(db, "products"));
+return querySnapshot.docs.map(doc => ({id: doc.id, ...doc.data()}));
+} catch (error) {
+throw error;
+}
 }
 
 const Homepage = () => {
-  const [loading, setLoading] = useState(true);
-  const [useArticle, setUseArticle] = useState([]);
-  const { products } = useFirebaseFeaturedProducts(5); // Fetch up to 5 featured products
+const [loading, setLoading] = useState(true);
+const [useArticle, setUseArticle] = useState([]);
+const { products } = useFirebaseFeaturedProducts(5); // Fetch up to 5 featured products
 
-  useEffect(() => {
-    const fetchProducts = async () => {
-      try {
-        const articles = await getArticles();
-        setUseArticle(articles);
-        setLoading(false);
-      } catch (error) {
-        setLoading(false);
-      }
-    };
-    fetchProducts();
-  }, []);
+useEffect(() => {
+const fetchProducts = async () => {
+try {
+const articles = await getArticles();
+setUseArticle(articles);
+setLoading(false);
+} catch (error) {
+setLoading(false);
+}
+};
+fetchProducts();
+}, []);
 
-  const mainProduct = useArticle.find(product => product.id === 'd4cY1CZ4eQnX26ipigAE');
-  const leftProducts = useArticle.filter(product => ['jTY4uUE9E5FYBM0rVBXr','cTAttMOYTSo2ccGvWFqJ'].includes(product.id));
-  const rightProducts = useArticle.filter(product => ['q7W8AVFUBjAg14Oy5sLW', 'G4BRbX5fnhJ5zUXoyya8'].includes(product.id));
-  const midProducts = useArticle.filter(product => ['swVsmBX5UVUH5o8ncG2m', 'NKkDvQWxxynRwvGRrT10'].includes(product.id));
+const mainProduct = useArticle.find(product => product.id === 'd4cY1CZ4eQnX26ipigAE');
+
+const leftProducts = useArticle.filter(product => ['jTY4uUE9E5FYBM0rVBXr','cTAttMOYTSo2ccGvWFqJ'].includes(product.id));
+
+const rightProducts = useArticle.filter(product => ['q7W8AVFUBjAg14Oy5sLW', 'G4BRbX5fnhJ5zUXoyya8'].includes(product.id));
+
+const midProducts = useArticle.filter(product => ['swVsmBX5UVUH5o8ncG2m', 'NKkDvQWxxynRwvGRrT10'].includes(product.id));
 
   
-  const formatPrice = (price) => {
-    return new Intl.NumberFormat('en-US', {
-      minimumFractionDigits: 0,
-      maximumFractionDigits: 1,
-    }).format(price);
-  };
-  return (
-    <>
-    <Carousel items={products}/>
-      <div className='hero-grid'>
-        {loading ? (
-          <div className="loader-container">
-            <RiseLoader color="blue" size={15} />
-          </div>
-        ) : (
-          <>
-            <div className="left-column">
-              {leftProducts.map((product) => (
-                <div key={product.id} className="first-left-content">
-                  <div className="content-wrapper">
-                    <span className='dashcategory'>{product.category}</span>
-                    <Link href={`/pages/ProductDetails/${product.id}`}>
-                      <img src={product.coverimage || product.cardisplay} alt={product.title} />
-                    </Link>
-                    <span style={{ fontSize: '20px', lineHeight: '40px', textAlign: 'center' }}>{product.title} {product.cartitle}</span>
-                    <div className='detailproduct-price'>               
-                             ${formatPrice(product.price || product.basePrice)}
-                    </div>
-                  </div>
-                </div>
-              ))}
-            </div>
+const formatPrice = (price) => {
+return new Intl.NumberFormat('en-US', {
+minimumFractionDigits: 0,
+maximumFractionDigits: 1,
+}).format(price);
+};
+return (
+<>
+<Carousel items={products}/>
+<div className='hero-grid'>
+{loading ? (
+<div className="loader-container">
+<RiseLoader color="blue" size={15} />
+</div>
+) : (
+<>
+<div className="left-column">
+{leftProducts.map((product) => (
+<div key={product.id} className="first-left-content">
+<div className="content-wrapper">
+<span className='dashcategory'>{product.category}</span>
+<Link href={`/pages/ProductDetails/${product.id}`}>
+<img src={product.coverimage || product.cardisplay} alt={product.title} />
+</Link>
+<span style={{ fontSize: '20px', lineHeight: '40px', textAlign: 'center' }}>{product.title} {product.cartitle}</span>
+<div className='detailproduct-price'>               
+${formatPrice(product.price || product.basePrice)}
+</div>
+</div>
+</div>
+))}
+</div>
 
-            {mainProduct && (
-              <div className="main-content">
-                <div className="mainflex">
-                  <div style={{ display: 'grid' }}>
-                    <span className='dashcategory'>{mainProduct.category}</span>
-                    <span className='detailproduct-price'>           
-                                 ${formatPrice(mainProduct.price)}
-                    </span>
-                  </div>
-                  <Link href={`/pages/ProductDetails/${mainProduct.id}`}>
-                    <img src={mainProduct.coverimage} className="main-content-img" alt={mainProduct.title} />
-                  </Link>
-                  <span style={{ fontSize: '17px', lineHeight: '40px' }}>{mainProduct.title}</span>
-                  <div style={{ width: '10rem', margin: 'auto' }}>
-                    <span>
-                    </span>
-                  </div>
-                </div>
-              </div>
-            )}
+{mainProduct && (
+<div className="main-content">
+<div className="mainflex">
+<div style={{ display: 'grid' }}>
+<span className='dashcategory'>{mainProduct.category}</span>
+<span className='detailproduct-price'>           
+${formatPrice(mainProduct.price)}
+</span>
+</div>
+<Link href={`/pages/ProductDetails/${mainProduct.id}`}>
+<img src={mainProduct.coverimage} className="main-content-img" alt={mainProduct.title} />
+</Link>
+<span style={{ fontSize: '17px', lineHeight: '40px' }}>{mainProduct.title}</span>
+<div style={{ width: '10rem', margin: 'auto' }}>
+<span>
+</span>
+</div>
+</div>
+</div>
+)}
 
-            <div className="right-column">
-              {rightProducts.map((product) => (
-                <div key={product.id} className="second-left-content">
-                  <div className="content-wrapper">
-                    <span className='dashcategory'>{product.category}</span>
-                    <Link href={`/pages/ProductDetails/${product.id}`}>
-                      <img src={product.coverimage} alt={product.title} />
-                    </Link>
-                    <span style={{ fontSize: '20px', lineHeight: '40px', textAlign: 'center' }}>{product.title} {product.cartitle}</span>
-                    <div className='detailproduct-price'>             
-                               ${formatPrice(product.price || product.basePrice)}
-                    </div>
-                  </div>
-                </div>
-              ))}
-            </div>
-          </>
-        )} 
-    
-      </div>
+<div className="right-column">
+{rightProducts.map((product) => (
+<div key={product.id} className="second-left-content">
+<div className="content-wrapper">
+<span className='dashcategory'>{product.category}</span>
+<Link href={`/pages/ProductDetails/${product.id}`}>
+<img src={product.coverimage} alt={product.title} />
+</Link>
+<span style={{ fontSize: '20px', lineHeight: '40px', textAlign: 'center' }}>{product.title} {product.cartitle}</span>
+<div className='detailproduct-price'>             
+${formatPrice(product.price || product.basePrice)}
+</div>
+</div>
+</div>
+))}
+</div>
+</>
+)} 
+</div>
 
 <div className="mid-column">
 {midProducts.map((product) => (
--- app/pages/Account/page.tsx
+++ app/pages/Account/page.tsx
@@ -1,7 +1,9 @@
-import Footer from '@/app/components/Footer'
-import Navbar from '@/app/components/Navbar'
+
 import { Metadata } from 'next'
 import React from 'react'
+import Navbar from '../../components/Navbar'
+import Footer from '../../components/Footer'
+import AccountPage from '../Account/AccountPage'
 export const metadata: Metadata = {
     title: 'Gulime Profile',
     description: 'Explore Gulime, your premier ecommerce destination for a vast array of products, including cars & Trucks. Discover great deals, fast shipping, and exceptional customer service. From everyday essentials to luxury items, Gulime has got you covered.',
@@ -11,6 +13,7 @@ export default function page() {
 return (
 <>
 <Navbar/>
+<AccountPage/>
 <Footer/>
 </>
 )
